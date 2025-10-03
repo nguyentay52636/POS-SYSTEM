14ec6341b0efab2ac951bd4be563f7410a34639d
tay52636 <phuongtay52636@gmail.com>
2025-10-03 14:08:26 +0700

    add Promotions UI

--- frontend/src/app/admin/promotions/page.tsx
+++ frontend/src/app/admin/promotions/page.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import ManagerPromotions from '@/components/Admin/components/Promotion/ManagerPromotions'
 
 export default function page() {
-  return (
-    <div>page</div>
-  )
+    return (
+        <>
+            <ManagerPromotions />
+        </>
+    )
 }
